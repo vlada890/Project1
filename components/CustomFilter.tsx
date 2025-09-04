b8e6d6c4bd82fb8fdf52d5e33425148b27b0b8f4
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-09-04 19:52:40 +0300

    Add files via upload

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type CustomFilterProps = {
+    title: string;
+};
+
+const CustomFilter = ({title}: CustomFilterProps) => {
+  return (
+    <div>CustomFilter</div>
+  )
+}
+
+export default CustomFilter
---
+++ components/Footer.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { footerLinks } from "@/constants";
+
+const Footer = () => (
+  <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
+    <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+      <div className='flex flex-col justify-start items-start gap-6'>
+        <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain' />
+        <p className='text-base text-gray-700'>
+          Carhub 2023 <br />
+          All Rights Reserved &copy;
+        </p>
+      </div>
+
+      <div className="footer__links">
+        {footerLinks.map((link) => (
+            <div key={link.title}
+            className="footer__link">
+                <h3 className="font-bold">{link.title}</h3>
+                {link.links.map((item) => (
+                    <Link 
+                    href={item.url}
+                    key={item.title}
+                    className="text-gray-500">
+                        {item.title}
+                    </Link>
+                ))}
+            </div>
+        ))}
+      </div>
+    </div>
+
+    <div className="flex justify-between items=center mt-10 border-gray-100 sm:px-16 px-6 py-10 border-t">
+        <Link href="/"
+        className="text-gray-500"> Privacy Policy </Link>
+        <Link href="/"
+        className="text-gray-500"> Terms of Use </Link>
+    </div>
+
+  </footer>
+);
+
+export default Footer;
