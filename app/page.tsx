e6d48767370aa2282a5ebf09bafb3281934786f6
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-09-04 19:50:34 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+import { fetchCars } from "@/utils";
+import { fuels, yearsOfProduction } from "@/constants";
+import { CarCard, SearchBar, CustomFilter, Hero } from "@/components";
+
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  return (
+    <main className='overflow-hidden'>
+      <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' />
+            <CustomFilter title='year' />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
