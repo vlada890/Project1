1d6413f745ff04399bfac6c8ccb480e996d794ca
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-09-05 09:35:14 +0300

    Update page.tsx for job

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,48 @@
-import { fetchCars } from "@/utils";
-import { fuels, yearsOfProduction } from "@/constants";
-import { CarCard, SearchBar, CustomFilter, Hero } from "@/components";
+import Image from 'next/image';
+import Hero from '@/components/Hero';
+import CustomFilter from '@/components/CustomFilter';
+import { SearchBar, JobCard } from '@/components';
+import { fetchJobs } from '@/utils';
 
 export default async function Home() {
-  const allCars = await fetchCars();
-
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
-
+  const allJobs = await fetchJobs();
+  const isDataEmpty = !Array.isArray(allJobs) || allJobs.length < 1 || !allJobs;
+  
   return (
     <main className='overflow-hidden'>
       <Hero />
-
+      
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-          <p>Explore out cars you might like</p>
+          <h1 className='text-4xl font-extrabold'>Job Listings</h1>
+          <p>Explore job opportunities you might like</p>
         </div>
-
+        
         <div className='home__filters'>
           <SearchBar />
-
+          
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <CustomFilter title='type' />
+            <CustomFilter title='level' />
           </div>
         </div>
-
-        {!isDataEmpty ? (
-          <section>
-            <div className='home__cars-wrapper'>
-              {allCars?.map((car) => (
-                <CarCard car={car} />
-              ))}
-            </div>
-
-          </section>
-        ) : (
-          <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
-            <p>{allCars?.message}</p>
+        
+      {!isDataEmpty ? (
+        <section>
+          <div className='home__jobs-wrapper'>
+            {allJobs.map((job, index) => (
+              <JobCard key={index} job={job}/> 
+            ))}
           </div>
-        )}
-      </div>
-    </main>
-  );
-}
+        </section>
+      ) : (
+        <div className='home__error-container'>
+          <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+          <p>No jobs found. Try adjusting your filters.</p>
+        </div>
+      )}
+
+            </div>
+          </main>
+        );
+      }
